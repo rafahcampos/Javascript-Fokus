4556ba6b11d3f22a6cf95a622d1f67fd136928d4
Rafael Campos <rafael.campos.m@hotmail.com>
2024-03-22 18:19:43 -0300

    Implementação do método localStorage

--- script-crud.js
+++ script-crud.js
@@ -3,8 +3,24 @@
 
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+
+const tarefas = [];
 
 btnAdicionarTarefa.addEventListener('click', () => {           
     formAdicionarTarefa.classList.toggle('hidden');         //faz com que o css exiba ou não alguma classe
 }); 
+
+formAdicionarTarefa.addEventListener('submit', (evento) =>{
+    evento.preventDefault();                             //impede o comportamento padrão
+    const tarefa ={
+        descricao : textArea.value,
+    }
+
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    
+
+    
+} );
 
